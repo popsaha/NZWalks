2160358528176ed65a5e766e45ee0538fc2d403e
Snehashish Saha <saha.snehashish@gmail.com>
2023-02-11 10:31:10 +0530

    adding user repository in dependency injection

--- NZWalks/NZWalks_API/Program.cs
+++ NZWalks/NZWalks_API/Program.cs
@@ -26,6 +26,8 @@ builder.Services.AddDbContext<NZWalksDbContext>(options =>
 builder.Services.AddScoped<IRegionRepository, RegionRepository>();
 builder.Services.AddScoped<IWalkRepository, WalkRepository>();
 
+builder.Services.AddSingleton<IUserRepository, StaticUserRepository>();
+
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters
